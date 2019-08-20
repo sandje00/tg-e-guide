379b4adf6fe15fd3744acae9c452b8e032761517
sandje00 <stella.andjelic.00@fesb.hr>
2019-08-20 22:29:01 +0200

    Client: Adding history mode resolves hash routing, but does not prevent always rendering the same page.

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,10 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
-const router = new VueRouter({routes})
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+})
 
 new Vue({
   router,
