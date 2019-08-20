59db14b27c13dc73db5b50e3401358f3bd73c961
sandje00 <stella.andjelic.00@fesb.hr>
2019-08-20 21:43:21 +0200

    Client: Vue router still not working, but a bit closer to the solution.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,13 +6,10 @@
 
 <script>
 export default {
-  data() {
-    return;
-  }
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,29 @@
-import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld';
+import Register from '@/components/Register';
+
+const routes = [
+    {
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register
+    }
+];
+
+export default routes;
+
+/* import Vue from 'vue';
 import router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Register from '@/components/Register';
 
 Vue.use(router);
 
-export default new Router({
+export default new VueRouter({
     routes: [
         {
             path: '/',
@@ -18,4 +36,4 @@ export default new Router({
             component: Register
         }
     ]
-});
+}); */
