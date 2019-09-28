ba8bb2c667bd07ae0ccc8efae7468de09cd8ed15
sandje00 <stella.andjelic.00@fesb.hr>
2019-09-28 11:46:12 +0200

    Client: State persisted after page refresh

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+});
 
 export default new Vuex.Store({
     strict: true,
@@ -31,5 +36,6 @@ export default new Vuex.Store({
         setUser({commit}, user) {
             commit('setUser', user);
         }
-    }
+    },
+    plugins: [vuexLocal.plugin]
 });
