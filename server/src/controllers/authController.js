a586241f42dcb7681a1a868e210067a4ff0a6cc7
sandje00 <stella.andjelic.00@fesb.hr>
2019-09-29 17:29:21 +0200

    Server: authController improved (better error handling)

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,6 +3,13 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 const {User} = require('../models');
 
+const ERROR = {
+    already_exists: `<b>This username is already in use.</b>`,
+    user_not_found: `<b>Incorrect username: no such user found.</b>`,
+    invalid_pwd: `<b>Your password is incorrect.</b>`,
+    server_error: `<b>An error has occured trying to sign in.</b>`
+}
+
 function jwtSignUser(user) {
     const DAY = 60 * 60 * 24;
     return jwt.sign(user, config.auth.jwtSecret, {
@@ -21,8 +28,8 @@ module.exports = {
                 }); 
             })
             .catch(err => {
-                res.status(400).send({
-                    error: `<b>This username is already in use.</b>`
+                res.status(409).send({
+                    error: ERROR.already_exists
                 });
             });
     },
@@ -30,34 +37,31 @@ module.exports = {
         try {
             const {username, hpwd} = req.body;
             const user = await User.findOne({
-                where: {
-                    username: username
-                }
+                where: { username }
             });
 
             if(!user) {
-                return res.status(403).send({
-                    error: `<b>There is no such user as ${username}.</b>`
+                return res.status(401).send({
+                    error: ERROR.user_not_found
                 });
             }
 
             const is_pwd_valid = await user.pwdcmp(hpwd);
             if(!is_pwd_valid) {
-                return res.status(403).send({
-                    error: `<b>Your password is incorrect.</b>`
+                return res.status(401).send({
+                    error: ERROR.invalid_pwd
                 });
             }
 
             const user_json = user.toJSON();
             res.send({
                 user: user_json,
-                token: jwtSignUser(user_json),
-                success: 'You have signed in successfully!'
+                token: jwtSignUser(user_json)
             });
         }
         catch(err) {
             res.status(500).send({
-                error: `<b>An error has occured trying to sign in.</b>`
+                error: err.message
             });
         }
     }
