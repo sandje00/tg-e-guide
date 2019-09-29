a700978b9614ec744fd2f43438812b27e86c7d2e
sandje00 <stella.andjelic.00@fesb.hr>
2019-09-29 10:53:32 +0200

    Server: Bug fix

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan = require('morgan');
+/* const morgan = require('morgan'); */
 
 const config = require('./config');
 const {sequelize} = require('./models');
 
 const app = express();
 
-app.use(morgan('combined'));
+/* app.use(morgan('combined')); */
 app.use(bodyParser.json());
 app.use(cors());
 
